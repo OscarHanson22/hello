103145908f26a2c9e3723d36a9adcce2debc6687
OscarHanson22 <ohanson5@huskers.unl.edu>
2024-09-02 20:45:14 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,20 @@
+/**
+ * Author: Oscar Hanson
+ * Email: ohanson5@huskers.unl.edu
+ *
+ * Date: Sep. 2, 2024
+ *
+ * A simple program to test
+ * basic Git functionality
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Oscar Hanson\n");
+
+    return 0;
+}
